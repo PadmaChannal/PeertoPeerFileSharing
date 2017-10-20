0354af2d75435dd108b20b5c6e42b5ee8b068e5b
Padmavati <padmahchannal@gmail.com>
2017-10-19 21:20:01 -0500

    Initial

--- app/src/main/java/com/example/padma/peertopeerfilesharing/MainActivity.java
+++ app/src/main/java/com/example/padma/peertopeerfilesharing/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-// ToDo
+// ToDo for wireless
 public class MainActivity extends AppCompatActivity {
 
     @Override
